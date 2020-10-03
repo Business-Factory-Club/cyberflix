85d45d6e48663f03a766af8ac2c337239d30904f
Lucas Ferreira <luckeiros@users.noreply.github.com>
2020-10-03 18:51:43 -0300

    creating implementation classes

--- app/src/main/java/club/businessfactory/cyberflix/data/repository/MockDiscoverRepository.kt
+++ app/src/main/java/club/businessfactory/cyberflix/data/repository/MockDiscoverRepository.kt
@@ -1,2 +1,4 @@
 package club.businessfactory.cyberflix.data.repository
 
+class MockDiscoverRepository {
+}
---
+++ app/src/main/java/club/businessfactory/cyberflix/data/repository/RetrofitDiscoverRepository.kt
@@ -0,0 +1,4 @@
+package club.businessfactory.cyberflix.data.repository
+
+class RetrofitDiscoverRepository {
+}
