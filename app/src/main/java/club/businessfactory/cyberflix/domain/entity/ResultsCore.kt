c3b3ecc4f177895c38afd4570a0f7e5baade049a
Lucas Ferreira <luckeiros@users.noreply.github.com>
2020-10-03 18:54:30 -0300

    creating core

---
+++ app/src/main/java/club/businessfactory/cyberflix/domain/entity/ResultsCore.kt
@@ -0,0 +1,10 @@
+package club.businessfactory.cyberflix.domain.entity
+
+data class ResultsCore (
+    val id: Int,
+    val voteAverage: Double,
+    val title: String,
+    val releaseDate: String,
+    val overview: String,
+    val posterPath: String
+)
