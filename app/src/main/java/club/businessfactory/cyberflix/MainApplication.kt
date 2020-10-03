4bad2e77eb1711fda02058f9b366766d3af41017
Pedro Teles <pedroateles@protonmail.com>
2020-10-03 15:24:15 -0300

    initial commit

---
+++ app/src/main/java/club/businessfactory/cyberflix/MainApplication.kt
@@ -0,0 +1,9 @@
+package club.businessfactory.cyberflix
+
+import android.app.Application
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/club/businessfactory/cyberflix/presentation/feature/home/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package club.businessfactory.cyberflix.presentation.feature.home.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import club.businessfactory.cyberflix.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
